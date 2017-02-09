b627ee77265e6c152ff3b9c0ee35fbbace07ddea
Saajan <saajan.sn@gmail.com>
2017-02-09 12:17:16 +0530

    table css changes

--- public/dist/scripts/main.js
+++ public/dist/scripts/main.js
@@ -4,8 +4,7 @@
   socket.on('success', function (data) {
     var fullData = data.data;
     if (fullData.status != null || fullData.status != undefined) {
-      var table = $('<table></table>').addClass('domain-table');
-      var tbody = $('<tbody></tbody>');
+      var tbody = $('.domain-table tbody');
       var tr = $('<tr></tr>');
       var td1 = $('<td></td>').text(fullData.domain);
       tr.append(td1);
@@ -13,13 +12,11 @@
       tr.append(td2);
       var td3 = $('<td></td>').text(fullData.status.success);
       tr.append(td3);
-      var td4 = $('<td></td>').text(fullData.status.unverified.toString());
+      var td4 = $('<td></td>').text(fullData.status.verified.toString());
       tr.append(td4);
-      var td5 = $('<td></td>').text(fullData.status.verified.toString());
+      var td5 = $('<td></td>').text(fullData.status.unverified.toString());
       tr.append(td5);
-      tbody.append(tr);
-      table.append(tbody);
-      $('.table-container').append(table);
+      $(tbody).append(tr);
     }
   });
 
