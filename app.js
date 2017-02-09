635a1f7fd90e0d9441c319b0d24d2d1505dad456
Saajan <saajan.sn@gmail.com>
2017-02-09 17:16:46 +0530

    csv writer

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
 // simple express server
 var express = require('express');
-var fs = require("fs");
+var fs = require('fs');
 var exphbs = require('express-handlebars');
-var bodyParser = require("body-parser");
+var bodyParser = require('body-parser');
 var app = express();
 var config = require('./config/config.js');
 var verify = require('bulk-email-verifier');
-var csv = require('csv-parser')
-var $ = require("jquery");
-var _ = require("underscore");
-
-
+var csv = require('csv-parser');
+var $ = require('jquery');
+var _ = require('underscore');
+var fileUpload = require('express-fileupload');
 var path = require('path');
+var csvWriter = require('csv-write-stream');
+var writer = csvWriter();
 
-app.set("views", path.join(__dirname, "views"));
+app.set('views', path.join(__dirname, 'views'));
 
 app.engine('handlebars', exphbs({
   defaultLayout: 'layout',
   layoutsDir: 'views/layouts/',
   partialsDir: 'views/partials/'
 }));
+
+app.use(fileUpload());
 
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({
@@ -34,13 +37,12 @@ var server = require('http').createServer(app);
 
 var io = require('socket.io').listen(server);
 
-require('./routes/routes.js')(express, app, fs, verify, csv, $,_,io);
+require('./routes/routes.js')(express, app, fs, verify, csv, $, _, io, writer);
 
 server.listen(process.env.PORT || 3000, function () {
   if (process.env.PORT !== undefined) {
-    console.log("Server is running at port " + process.env.PORT);
+    console.log('Server is running at port ' + process.env.PORT);
   } else {
-    console.log("Server is running at port");
+    console.log('Server is running at port');
   }
-
 });
--- routes/routes.js
+++ routes/routes.js
@@ -1,40 +1,64 @@
-module.exports = function(express, app, fs, verify, csv, $, _, io) {
+module.exports = function (express, app, fs, verify, csv, $, _, io, writer) {
   var router = express.Router();
   require('events').EventEmitter.defaultMaxListeners = Infinity;
 
-  router.get('/', function(req, res, next) {
-    var domains = [];
-    var emails = [];
-    fs.createReadStream('public/dist/static/email.csv')
-      .pipe(csv())
-      .on('data', function(data) {
-        emails.push(data.Emails);
-        domains.push((data.Emails).split('@')[1]);
-      }).on('end', function() {
-        var uniqueDomain = _.uniq(domains);
-        _.map(uniqueDomain, function(value) {
-          var emailsArray = [];
-          _.map(emails, function(value1) {
-            if (value1.endsWith(value)) {
-              emailsArray.push(value1);
-            }
-          });
-          io.on('connection', function(socket) {
-            verify.verifyEmails(value, emailsArray, {}, function(err, data) {
-              var finalObj = {
-                domain: value,
-                email: emailsArray,
-                status: data,
-                error: err
-              };
-              socket.emit('success', {
-                data: finalObj
+  router.post('/upload', function (req, res, next) {
+    var sampleFile;
+    if (!req.files) {
+      res.send('No files were uploaded.');
+      return;
+    }
+    sampleFile = req.files.sampleFile;
+    sampleFile.mv('public/dist/static/email.csv', function (err) {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        var domains = [];
+        var emails = [];
+        fs.createReadStream('public/dist/static/email.csv')
+          .pipe(csv())
+          .on('data', function (data) {
+            console.log(data);
+            emails.push(data.Emails);
+            domains.push((data.Emails).split('@')[1]);
+          }).on('end', function () {
+            var uniqueDomain = _.uniq(domains);
+            _.map(uniqueDomain, function (value) {
+              var emailsArray = [];
+              _.map(emails, function (value1) {
+                if (value1.endsWith(value)) {
+                  emailsArray.push(value1);
+                }
+              });
+              io.on('connection', function (socket) {
+                writer.pipe(fs.createWriteStream('public/dist/static/verified.csv'));
+                verify.verifyEmails(value, emailsArray, {}, function (err, data) {
+                  writer.write({
+                    email: data.verified
+                  });
+                  var finalObj = {
+                    domain: value,
+                    email: emailsArray,
+                    status: data,
+                    error: err
+                  };
+                  socket.emit('success', {
+                    data: finalObj
+                  });
+                });
+                writer.end();
               });
             });
           });
-        });
-      });
+      }
+    });
 
+    res.render('upload', {
+      helpers: {}
+    });
+  });
+
+  router.get('/', function (req, res, next) {
     res.render('index', {
       helpers: {}
     });
