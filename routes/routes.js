948585a3bfb6370352fa6e541008a54ce67b93bf
Saajan <saajan.sn@gmail.com>
2017-03-15 00:10:23 +0530

    my email validity

--- routes/routes.js
+++ routes/routes.js
@@ -71,7 +71,7 @@ const _isValidDomainMX = (domains, emails) => {
   //   }
   //  });
   // });
-  dns.resolve(emails[0], (error, addresses) => { console.error(error); console.log(addresses); });
+  dns.resolve('saajan.sn@gmail.com', (error, addresses) => { console.error(error); console.log(addresses); });
   domains.forEach(function (domain,index) {
     //verify.verifyEmails(domain, emails[index], {}, function (err, data) {
       //console.log(data);
