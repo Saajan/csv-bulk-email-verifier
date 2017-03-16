c93f8fa7c89339572524a5f6723d52af3d12e9a3
Saajan <saajan.sn@gmail.com>
2017-03-17 01:59:44 +0530

    domain absent count added

--- src/scripts/custom/main.js
+++ src/scripts/custom/main.js
@@ -1,19 +1,19 @@
-(function() {
+(function () {
   var socket = io.connect('https://digitvalue2.herokuapp.com/');
   // var socket = io.connect('http://localhost:3000');
   var jsonData = [];
   var index = 0;
-  socket.on('success', function(data) {
+  socket.on('success', function (data) {
     var fullData = data.data;
     if (fullData.status != null || fullData.status != undefined) {
-      if (fullData.status.verified !== undefined) {
+      if ((fullData.status.verified).length > 0) {
         // if (fullData.status.verified == '') {
         console.log(fullData);
         // $.each(fullData.status.verified, function (key, value) {
         var json = {
           email: fullData.email
         };
-        
+
         index++;
         jsonData.push(json);
         // });
@@ -34,6 +34,22 @@
       var td5 = $('<td></td>').text(fullData.status.unverified);
       tr.append(td5);
       $(tbody).append(tr);
+    } else if (fullData.error != null) {
+      var tbody = $('.domain-table tbody');
+      var tr = $('<tr></tr>');
+      var td11 = $('<td></td>').text("-");
+      tr.append(td11);
+      var td1 = $('<td></td>').text(fullData.domain);
+      tr.append(td1);
+      var td2 = $('<td></td>').text(fullData.email);
+      tr.append(td2);
+      var td3 = $('<td></td>').text("domain absent");
+      tr.append(td3);
+      var td4 = $('<td></td>').text("");
+      tr.append(td4);
+      var td5 = $('<td></td>').text("");
+      tr.append(td5);
+      $(tbody).append(tr);
     }
   });
 
@@ -48,14 +64,14 @@
     result = '';
     result += keys;
     result += lineDelimiter;
-    data.forEach(function(item) {
+    data.forEach(function (item) {
       result += item.email;
       result += lineDelimiter;
     });
     return result;
   }
 
-  $('#csvid').on('click', function() {
+  $('#csvid').on('click', function () {
     var data, filename, link;
     var csv = convertArrayOfObjectsToCSV({
       data: jsonData
