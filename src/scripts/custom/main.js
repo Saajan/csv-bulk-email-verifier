5e2856dcabb5fdaeb80c26eb4101c137395b86be
Saajan <saajan.sn@gmail.com>
2017-03-18 02:34:19 +0530

    new strategy

--- src/scripts/custom/main.js
+++ src/scripts/custom/main.js
@@ -1,56 +1,56 @@
 (function () {
   var socket = io.connect('https://digitvalue.herokuapp.com/');
   // var socket = io.connect('http://localhost:3000');
-  var jsonData = [];
-  var index = 0;
+  var fullDatum = [];
   socket.on('success', function (data) {
-    var fullData = data.data;
-    console.log(fullData);
-    index++;
-    if (fullData.status != null || fullData.status != undefined) {
-      if ((fullData.status.verified).length > 0) {
-        // if (fullData.status.verified == '') {
-        //console.log(fullData);
-        // $.each(fullData.status.verified, function (key, value) {
-        var json = {
-          email: fullData.email
-        };
-        jsonData.push(json);
-        // });
-        // }
+    var fullDatum = data.data;
+    console.log(fullDatum);
+    fullDatum.forEach(function (fullData, index) {
+      if (fullData.status != null || fullData.status != undefined) {
+        if ((fullData.status.verified).length > 0) {
+          // if (fullData.status.verified == '') {
+          //console.log(fullData);
+          // $.each(fullData.status.verified, function (key, value) {
+          var json = {
+            email: fullData.email
+          };
+          jsonData.push(json);
+          // });
+          // }
+        }
+        var tbody = $('.domain-table tbody');
+        var tr = $('<tr></tr>');
+        var td11 = $('<td></td>').text(index);
+        tr.append(td11);
+        var td1 = $('<td></td>').text(fullData.domain);
+        tr.append(td1);
+        var td2 = $('<td></td>').text(fullData.email);
+        tr.append(td2);
+        var td3 = $('<td></td>').text(fullData.status.success);
+        tr.append(td3);
+        var td4 = $('<td></td>').text(fullData.status.verified);
+        tr.append(td4);
+        var td5 = $('<td></td>').text(fullData.status.unverified);
+        tr.append(td5);
+        $(tbody).append(tr);
+      } else if (fullData.error != null) {
+        var tbody = $('.domain-table tbody');
+        var tr = $('<tr></tr>');
+        var td11 = $('<td></td>').text(index);
+        tr.append(td11);
+        var td1 = $('<td></td>').text(fullData.domain);
+        tr.append(td1);
+        var td2 = $('<td></td>').text(fullData.email);
+        tr.append(td2);
+        var td3 = $('<td></td>').text("domain absent");
+        tr.append(td3);
+        var td4 = $('<td></td>').text("");
+        tr.append(td4);
+        var td5 = $('<td></td>').text("");
+        tr.append(td5);
+        $(tbody).append(tr);
       }
-      var tbody = $('.domain-table tbody');
-      var tr = $('<tr></tr>');
-      var td11 = $('<td></td>').text(index);
-      tr.append(td11);
-      var td1 = $('<td></td>').text(fullData.domain);
-      tr.append(td1);
-      var td2 = $('<td></td>').text(fullData.email);
-      tr.append(td2);
-      var td3 = $('<td></td>').text(fullData.status.success);
-      tr.append(td3);
-      var td4 = $('<td></td>').text(fullData.status.verified);
-      tr.append(td4);
-      var td5 = $('<td></td>').text(fullData.status.unverified);
-      tr.append(td5);
-      $(tbody).append(tr);
-    } else if (fullData.error != null) {
-      var tbody = $('.domain-table tbody');
-      var tr = $('<tr></tr>');
-      var td11 = $('<td></td>').text(index);
-      tr.append(td11);
-      var td1 = $('<td></td>').text(fullData.domain);
-      tr.append(td1);
-      var td2 = $('<td></td>').text(fullData.email);
-      tr.append(td2);
-      var td3 = $('<td></td>').text("domain absent");
-      tr.append(td3);
-      var td4 = $('<td></td>').text("");
-      tr.append(td4);
-      var td5 = $('<td></td>').text("");
-      tr.append(td5);
-      $(tbody).append(tr);
-    }
+    });
   });
 
   function convertArrayOfObjectsToCSV(args) {
